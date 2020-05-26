cf5922a0ed0e74e7fce45f99790feeceb745f3a5
denpufa <47856330+denpufa@users.noreply.github.com>
2020-05-26 02:55:29 -0300

    continuing chapter one

---
+++ Fundamentals/comment.js
@@ -0,0 +1,19 @@
+//comment for one line
+console.log('line 1')
+
+/*
+comment 
+mutiple
+lines
+*/
+console.log('line 2')
+
+/*
+ *many
+ *forms
+ *to do
+*/
+console.log('line 3')
+
+
+
---
+++ Fundamentals/variablesandconst.js
@@ -0,0 +1,18 @@
+var one = 2
+let two = 3
+
+//javascript curiosity
+var one = 2
+two = 300
+
+console.log(one,two)
+
+one = 4
+two = 200
+
+console.log(one,two)
+
+//always try use const for variables that don't change
+const name = 'rodolfo'
+
+console.log(name)
